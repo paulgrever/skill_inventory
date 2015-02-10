580ccd16b8eb14e6bc85688b14229a4fcc766790
Paul Grever <paulgrever@gmail.com>
2015-02-10 15:42:14 -0700

    made a skill list with crud with dj

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,9 @@
 class Skill
-  
+  attr_reader :id, :title, :description
+
+  def initialize(params)
+    @id = params["id"]
+    @title = params["title"]
+    @description = params["description"]
+  end
 end
